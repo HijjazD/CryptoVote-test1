726152cf2732cf8b36359fb63bb3079182280ac8
HijjazD <154423459+HijjazD@users.noreply.github.com>
2025-04-07 22:45:55 +0800

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,17 @@
+ const express = require('express')
+ const app = express()
+
+ app.use("/", (req, res) => {
+    res.send("hello world!!")
+ })
+
+ app.listen(5000, 
+    console.log("server on port 5000")
+ )
+
+
+
+
+
+
+
